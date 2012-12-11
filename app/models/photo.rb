01c4fbbb55af72a2ced87bf04d373777f132e527
Oksana Dushenkovskaya <oksana@oksana-laptop.(none)>
2012-12-11 19:05:58 +0200

    students added and fix photo gallery

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
+  has_attached_file :picture#,:styles => { :original => "558x524>", :small => "150x150>"},:content_type => "image/*",
+  #:size => { :in => 20..20000.kilobytes }
   attr_accessible :description, :id_albums, :picture
 end
