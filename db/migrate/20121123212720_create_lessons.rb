37637620132f5028093544005d2b60d59d47a1a8
arthur12345543 <arthur952010@gmail.com>
2012-12-21 18:42:01 +0400

    Delete multiple migrations CreateLessons

--- db/migrate/20121123212720_create_lessons.rb
+++
@@ -1,9 +0,0 @@
-class CreateLessons < ActiveRecord::Migration
-  def change
-    create_table :lessons do |t|
-      t.text :tittle, :maximum=>100
-
-      t.timestamps :maximum=>100
-    end
-  end
-end
