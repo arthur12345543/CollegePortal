850ffe8453fe1561337559a73ee8433129fec21d
Nikdev <nikdev@ubuntu.ubuntu-domain>
2013-01-13 07:47:15 +0700

    heroku expresions fix

--- db/migrate/20130111184737_create_roles.rb
+++ db/migrate/20130111184737_create_roles.rb
@@ -3,10 +3,10 @@ class CreateRoles < ActiveRecord::Migration
     create_table:roles do |t|
       t.string  :name
       t.string  :information
-      t.boolean :can_add_files, :default => 0
-      t.boolean :can_delete_files, :default => 0
-      t.boolean :can_add_news, :default => 0
-      t.boolean :can_delete_news, :default => 0
+      t.boolean :can_add_files, :default => true
+      t.boolean :can_delete_files, :default => true
+      t.boolean :can_add_news, :default => true
+      t.boolean :can_delete_news, :default => true
     end
   end
   def down
